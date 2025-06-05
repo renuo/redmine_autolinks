5bbe4d1dff485ea5a67b714682d60bd90188f314
Chris <76159444+hunchr@users.noreply.github.com>
2025-06-05 15:12:27 +0000

    Initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Redmine::Plugin.register :redmine_autolinks do
+  name "Autolinks"
+  author "hunchr"
+  description "Create custom references to external tickets."
+  version "0.1.0"
+  url "https://github.com/renuo/redmine_autolinks"
+  author_url "https://github.com/renuo"
+end
