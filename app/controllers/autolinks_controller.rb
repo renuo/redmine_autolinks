d54b0349738ac49a45392a8702db4b38db26f981
Chris <76159444+hunchr@users.noreply.github.com>
2025-06-10 09:48:51 +0000

    Add CRUD

--- app/controllers/autolinks_controller.rb
+++ app/controllers/autolinks_controller.rb
@@ -1,17 +1,53 @@
 # frozen_string_literal: true
 
 class AutolinksController < ApplicationController
-  def index; end
+  before_action :set_project
+  before_action :set_autolink, only: %i[show edit update destroy]
+
+  def index
+    @autolinks = Autolink.where(project: @project)
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @autolink = Autolink.new(project: @project)
+  end
 
-  def edit; end
+  def create
+    @autolink = Autolink.new(**autolink_params, project: @project)
 
-  def create; end
+    if @autolink.save
+      redirect_to project_autolink_path(@project, @autolink)
+    else
+      render :new
+    end
+  end
 
-  def update; end
+  def update
+    if @autolink.update(autolink_params)
+      redirect_to project_autolink_path(@project, @autolink)
+    else
+      render :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @autolink.destroy!
+    redirect_to project_autolinks_path
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_autolink
+    @autolink = Autolink.find_by(id: params[:id], project: @project)
+  end
+
+  def autolink_params
+    params.require(:autolink).permit(:prefix, :target_url)
+  end
 end
